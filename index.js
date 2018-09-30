9febe8ab8dcb5b453fd63938b17d301277e48b4c
bkfwebdev <bryant.feld@gmail.com>
2018-09-30 15:34:25 -0400

    request handlers added, not tested

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /*
+ * 
  * Primary file for API
  *
  */
@@ -6,6 +7,7 @@
 // Dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
  // Configure the server to respond to all requests with a string
 var server = http.createServer(function(req,res){
   
@@ -25,15 +27,76 @@ var method = req.method.toLowerCase();
 //get the headers as an object
 var headers = req.headers;
 
-// send the response
-res.end('Hello World!\n');
+// get payload if any
+decoder = new StringDecoder("utf-8");
+var buffer = "";
+req.on("data",function(data){
+buffer += decoder.write(data);
+});
 
-// log the request path
-console.log("the request was received with these headers",headers);
+req.on("end",function(){
+  buffer += decoder.end();
+//choose the handler this request should go to
+//if one is not found use the not found handler
+var chooseHandler = typeof(router[trimmedPath]) !== "undefined" ? rounter[trimmedPath] : theHandlers.notFound;
+
+//construct the data object to send to the handler
+var data {
+
+  "trimmedPath" : trimmedPath,
+  "queryStringObject" : queryStringObject,
+  "method" : method,
+  "headers" : headers,
+  "payload" : buffer
+
+};
+
+//route the request to handler specified in the router
+ chooseHandler(data,function(statusCode,payload){
+   //use the status code called back by the handler or default to 200
+   statusCode = typeof(statusCode) == "number" ? statusCode : 200;
+
+   //use the payload called back by the handler or default to an empty object
+   payload = typeof(payload) == "object" ? payload : {};
+
+   //convert the payload to a string
+   payloadString = JSON.stringify(payload);
+
+   //return the response
+   res.writeHead(statusCode);
+   res.end(payloadString);
+
+   // log the request path
+console.log("returning this response:", statusCode, payloadString);
+ });
+
+
+});
+
+
 });
 
 // Start the server
 server.listen(3000,function(){
   console.log('The server is up and running now');
 });
+
+// define handlers
+var theHandlers = {};
+
+//sample handler
+theHandlers.sample = function(data,callback){
+  // callback an http status code and a payload object
+  callback(406,{'name':'sample handler'});
+};
+
+//not found handler
+theHandlers.notFound = function(data,callback){
+  callback(404);
+};
+
+// define router
+var theRouter = {
+  "sample":handlers.sample
+}
 
